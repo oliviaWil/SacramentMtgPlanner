2866c6a89c370e9e73ef6ddc918fe6937b49ba77
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-04-10 10:55:34 -0600

    fixed details and added speaker page is added

---
+++ SacramentMtgPlanner/Pages/Meetings/Speakers.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SacramentMtgPlanner.Data;
+using SacramentMtgPlanner.Models;
+
+namespace SacramentMtgPlanner.Pages.Meetings
+{
+    public class SpeakersModel : PageModel
+    {
+        private readonly SacramentMtgPlanner.Data.SacramentMtgPlannerContext _context;
+
+        public SpeakersModel(SacramentMtgPlanner.Data.SacramentMtgPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Meeting> Meeting { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Meeting = await _context.Meeting.ToListAsync();
+        }
+    }
+}
